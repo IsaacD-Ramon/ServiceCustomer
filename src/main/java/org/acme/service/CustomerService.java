15c58d8fb6e5a48a4c32ca06974494f9509e98b9
IsaacD-Ramon <isaacramondg@hotmail.com>
2024-05-23 14:01:20 -0300

    classe controller e crud customer

--- src/main/java/org/acme/service/CustomerService.java
+++ src/main/java/org/acme/service/CustomerService.java
@@ -25,6 +25,33 @@ public class CustomerService {
         return customerDTOS;
     }
 
+    public void createNewCustomer(CustomerDTO customerDTO){
+        repository.persist(mapCustomerDTOToEntity(customerDTO));
+    }
+
+    public void changeCustomer(Long id, CustomerDTO customerDTO){
+
+        CustomerEntity customerEntity = repository.findById(id);
+        customerEntity = mapCustomerDTOToEntity(customerDTO);
+        repository.persist(customerEntity);
+    }
+
+    public void deleteCustomer(Long id){
+
+        repository.deleteById(id);
+    }
+
+    private CustomerEntity mapCustomerDTOToEntity(CustomerDTO customerDTO){
+        CustomerEntity customer = new CustomerEntity();
+
+        customer.setEmail(customerDTO.getEmail());
+        customer.setPhone(customerDTO.getPhone());
+        customer.setAddress(customerDTO.getAddress());
+        customer.setAge(customer.getAge());
+        customer.setName(customerDTO.getName());
+
+        return  customer;
+    }
     private CustomerDTO mapCustomerEntityToDTO(CustomerEntity customerEntity){
 
         CustomerDTO customerDTO = new CustomerDTO();
